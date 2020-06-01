fb97a97a98b56dbc6ee9d4661fc303c178dfc540
CarlosGaribay <carlosgaribay224@gmail.com>
2020-06-01 11:13:47 -0500

    mas sobre page_view

--- lib/enrutamiento_web/views/page_view.ex
+++ lib/enrutamiento_web/views/page_view.ex
@@ -18,4 +18,8 @@ defmodule EnrutamientoWeb.PageView do
   def render("index.html", assigns) do
     "rendering with assigns #{inspect Map.keys(assigns)}"
   end
+
+  def message do
+    "Hello from the view!"
+  end
 end
